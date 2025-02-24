9cbca8d25bf363d8972121a55f0efb7050534a54
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-02-24 09:49:32 +0100

    Temporarily disable `create mkdocs.yml` step

--- tools/prepare_docs.sh
+++ tools/prepare_docs.sh
@@ -5,17 +5,19 @@ cp -R ../assets-docs/overrides/ overrides/
 
 echo "\033[0;33m:::::: Add the extra files from the ../assets-branding\033[0m"
 mkdir -p docs/assets/images/
-cp ../assets-branding/EasyDiffraction/logos/ed-logo_dark.svg docs/assets/images/
-cp ../assets-branding/EasyDiffraction/logos/ed-logo_light.svg docs/assets/images/
-cp ../assets-branding/EasyDiffraction/logos/edl-logo_dark.svg docs/assets/images/logo_dark.svg
-cp ../assets-branding/EasyDiffraction/logos/edl-logo_light.svg docs/assets/images/logo_light.svg
-cp ../assets-branding/EasyDiffraction/icons/ed-icon_256x256.png docs/assets/images/favicon.png
+cp ../assets-branding/easydiffraction/hero/dark.png docs/assets/images/dark_light.png
+cp ../assets-branding/easydiffraction/hero/light.png docs/assets/images/hero_light.png
+cp ../assets-branding/easydiffraction/logos/dark.svg docs/assets/images/ed-logo_dark.svg
+cp ../assets-branding/easydiffraction/logos/light.svg docs/assets/images/ed-logo_light.svg
+cp ../assets-branding/easydiffraction/logos/dark.svg docs/assets/images/logo_dark.svg
+cp ../assets-branding/easydiffraction/logos/light.svg docs/assets/images/logo_light.svg
+cp ../assets-branding/easydiffraction/icons/ed-icon_256x256.png docs/assets/images/favicon.png
 mkdir -p overrides/.icons/
-cp ../assets-branding/EasyDiffraction/icons/ed-icon_bw.svg overrides/.icons/easydiffraction.svg
-cp ../assets-branding/EasyScienceOrg/icons/eso-icon_bw.svg overrides/.icons/easyscience.svg
+cp ../assets-branding/easydiffraction/icons/ed-icon_bw.svg overrides/.icons/easydiffraction.svg
+cp ../assets-branding/easyscience-org/icons/eso-icon_bw.svg overrides/.icons/easyscience.svg
 
 echo "\033[0;33m:::::: Add Jupyter notebooks from the project to the docs/\033[0m"
 cp -R examples/ docs/examples/
 
 echo "\033[0;33m:::::: Create the mkdocs.yml configuration file\033[0m"
-cat ../assets-docs/mkdocs.yml docs/mkdocs.yml > mkdocs.yml
+python tools/create_mkdocs-yml.py
