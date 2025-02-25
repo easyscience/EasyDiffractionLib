7bb7c53d08f4b95b2ea2fb00fa3af18dec44bdb1
Andrew Sazonov <andrew.sazonov@icloud.com>
2025-02-25 11:27:33 +0100

    Update __init__.py

--- src/easydiffraction/__init__.py
+++ src/easydiffraction/__init__.py
@@ -1,2 +1,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2025 EasySciene contributors (https://github.com/easyscience).
+
+# Aliases for the main classes and methods to simplify the usage
+from .job.job import DiffractionJob as Job
+from .job.model.phase import Phase
+from .utils import download_from_repository
+
+# Globals, which are imported from easydiffraction by a star import
+__all__ = ['Job', 'Phase', 'download_from_repository']
